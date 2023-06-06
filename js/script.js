5d66bec55dcd45df762dba254a42d8b8623107a3
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-06-06 10:48:57 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const bird = {
+  name: "Eliza",
+  type: "Budgie",
+  hungerScale: 5, // Scale from 0 (full) to 10 (starving)
+  isHappy: function () {
+    if (this.hungerScale <= 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+bird.eat = function () {
+  if (this.hungerScale === 0) {
+    console.log(`${this.name} is full to the brim. No food needed.`);
+  } else {
+    this.hungerScale -= 1;
+    console.log("nom nom nom nom nom");
+  }
+};
+
+bird.eat();
+console.log(bird.isHappy());
+bird.eat();
+console.log(bird.isHappy());
